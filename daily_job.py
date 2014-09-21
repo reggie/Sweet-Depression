a5d64e7366effa44d17dd5097fb0cd9034e5cb92
Manuel Lopez <manny.lopez625@gmail.com>
2014-09-21 05:54:46 -0400

    detect sadness

--- daily_job.py
+++ daily_job.py
@@ -2,6 +2,28 @@ import twitter
 from pymongo import MongoClient
 from key import consumer_key, consumer_secret, access_token_key, access_token_secret
 
+def get_tweets(user):
+    api = twitter.Api(consumer_key=consumer_key,
+            consumer_secret=consumer_secret,
+            access_token_key=access_token_key,
+            access_token_secret=access_token_secret)
+
+    statuses = api.GetUserTimeline(screen_name=user["twitter"])
+    for s in statuses[:3]:
+        print s.text
+        if sad_tweet(s.text):
+            send_order(user)
+
+def sad_tweet(tweet):
+    sadness = [":(", "):", ":'(",")':", "hack"]
+    for s in sadness:
+        if s in tweet:
+            return True
+    return False
+
+def send_order(user):
+    print 'sending order'    
+
 client = MongoClient()
 db = client['ordrin']
 user_collection = db['users']
@@ -9,19 +31,3 @@ user_collection = db['users']
 users = user_collection.find()
 for user in users:
     get_tweets(user)
-
-def get_tweets(user):
-
-    api = twitter.Api(consumer_key=consumer_key,
-            consumer_secret=consumer_secret,
-            access_token_key=access_token_key,
-            access_token_secret=access_token_secret)
-
-    statuses = api.GetUserTimeline(screen_name=user.twitter)
-    print [s.text for s in statuses]
-
-def analyze_tweet(tweet):
-
-
-def send_order(user):
-
