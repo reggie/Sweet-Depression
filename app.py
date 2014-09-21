f85c76e88c2806fd073b1f7b7f116f2a7decd926
Manuel Lopez <manny.lopez625@gmail.com>
2014-09-21 14:19:41 -0400

    placing order works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
 from key import  ordrin_secret
 import ordrin
 app = Flask(__name__) 
--- daily_job.py
+++ daily_job.py
@@ -1,5 +1,8 @@
 import twitter
 import ordrin
+import requests
+
+import simplejson
 from pymongo import MongoClient
 from key import consumer_key, consumer_secret, access_token_key, access_token_secret, ordrin_secret
 
@@ -30,16 +33,44 @@ def sad_tweet(tweet):
             return True
     return False
 
-def send_order(user):    
+def find_restaurant(user):    
+
     ordrin_api = ordrin.APIs(ordrin_secret, ordrin.TEST) 
+    print user["email"]
     address = ordrin_api.get_saved_addr(user["email"], "address", "password") 
+    print 'sqisdfasdf'
     cc = ordrin_api.get_saved_cc(user["email"], "card", "password")
-    print address
-    print cc
+
+    delivery_list = ordrin_api.delivery_list("ASAP", 
+                                            address["addr"], 
+                                            address["city"], 
+                                            address["zip"])
+
+    for restaurant in delivery_list:
+        rid = str(restaurant["id"])
+        args = {"rid": rid, "target": "cake", "size": "3"}
+        details = requests.get('http://foodbot.ordr.in:8000/TextSearch', 
+                                params=args)
+        tray = find_dessert(details)
+        if tray:
+            order_food(tray, rid, user, ordrin_api)
+    return False
+
+def find_dessert(details):
+    if details:
+        print details.text
+        details_list = simplejson.loads(details.text)
+        tray = details_list[0]['tray']
+        return tray 
+    return False
+
+def order_food(tray, rid, user, ordrin_api):
+    order = ordrin_api.order_user(rid, tray, 1.00, "Zerxsis", "the accusser", user['email'], "password", "address", "card", "ASAP") 
+    print order
 
 user = {"email": "mannyjl625@aol.com", "twitter": "unordrin"}
-send_order(user)
-"""
+find_restaurant(user)
+
 client = MongoClient()
 db = client['ordrin']
 user_collection = db['users']
@@ -47,4 +78,3 @@ user_collection = db['users']
 users = user_collection.find()
 for user in users:
     get_tweets(user)
-"""
