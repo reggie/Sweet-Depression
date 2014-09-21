4c1969204d2917dcfe12a0876b28a1efb814a9a0
Manuel Lopez <manny.lopez625@gmail.com>
2014-09-21 04:55:11 -0400

    schema for mongo and function defs

--- daily_job.py
+++ daily_job.py
@@ -1,11 +1,27 @@
 import twitter
+from pymongo import MongoClient
 from key import consumer_key, consumer_secret, access_token_key, access_token_secret
 
-api = twitter.Api(consumer_key=consumer_key,
-        consumer_secret=consumer_secret,
-        access_token_key=access_token_key,
-        access_token_secret=access_token_secret)
+client = MongoClient()
+db = client['ordrin']
+user_collection = db['users']
 
-statuses = api.GetUserTimeline(screen_name='unordrin')
+users = user_collection.find()
+for user in users:
+    get_tweets(user)
 
-print [s.text for s in statuses]
+def get_tweets(user):
+
+    api = twitter.Api(consumer_key=consumer_key,
+            consumer_secret=consumer_secret,
+            access_token_key=access_token_key,
+            access_token_secret=access_token_secret)
+
+    statuses = api.GetUserTimeline(screen_name=user.twitter)
+    print [s.text for s in statuses]
+
+def analyze_tweet(tweet):
+
+
+def send_order(user):
+
